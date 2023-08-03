123244d123db42e1d5402c3f88bd45958719c9af
emeraldhycient <igwezehycient86@gmail.com>
2023-08-03 13:18:39 +0100

    [update] fixed bug that inverts to default of usd

--- src/helpers/formatCurrency.ts
+++ src/helpers/formatCurrency.ts
@@ -3,9 +3,9 @@ import useAuthenticationState from "../states/authentication";
 //const locale = 'en-US'; // Use a specific locale, like 'en-US', 'fr-FR', 'de-DE', etc.
 const storeData = useAuthenticationState.getState();
 
-export const formatCurrency = ({ amount, locale = `en-${storeData?.authentication?.user?.location?.country_code}` }: { amount: number, locale?: string }) => {
-    console.log(locale)
-    const currencyCode = locale === 'en-NG' ? 'NGN' :"USD";
+export const formatCurrency = ({ amount, locale }: { amount: number, locale?: string }) => {
+    let locale2 = `en-${storeData?.authentication?.user?.location?.country_code || 'NG'}`
+    const currencyCode = locale === 'en-NG' || locale2 === 'en-NG' ? 'NGN' : "USD";
     const currencyStyle = 'symbol'; // Other options: 'code', 'name'
     const minimumFractionDigits = 2;
     const maximumFractionDigits = 2;
@@ -18,6 +18,6 @@ export const formatCurrency = ({ amount, locale = `en-${storeData?.authenticatio
         maximumFractionDigits,
     };
 
-    const formattedCurrency = new Intl.NumberFormat(locale,options).format(amount)
+    const formattedCurrency = new Intl.NumberFormat(locale ?? locale2,options).format(amount)
     return formattedCurrency;
 }
